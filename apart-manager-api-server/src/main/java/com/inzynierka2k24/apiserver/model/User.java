0932d138c9e0ac0a1d5f031fb1b91223c8803039
Michał Kaźmierczak <mkazmierczak112@gmail.com>
2023-10-03 16:07:00 +0200

    Refactored roles for user
    Co-authored-by: SnicketPolska <56560560+SnicketPolska@users.noreply.github.com>

--- apart-manager-api-server/src/main/java/com/inzynierka2k24/apiserver/model/User.java
+++ apart-manager-api-server/src/main/java/com/inzynierka2k24/apiserver/model/User.java
@@ -3,7 +3,7 @@ package com.inzynierka2k24.apiserver.model;
 import java.util.Optional;
 
 public record User(
-    Optional<Long> id, String login, String password, String mail, boolean active, String roles) {
+    Optional<Long> id, String login, String password, String mail, boolean active, List<String> roles) {
 
   public User(String login, String password, String mail) {
     this(Optional.of(0L), login, password, mail, true, "USER");
