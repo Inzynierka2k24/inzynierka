3cd5fa800ed954588e6c6e7be221eb74193cf36a
Krzysztof Szuba <krzysiu.sz120@gmail.com>
2023-10-28 16:01:22 +0200

    Exception handler changes

--- apart-manager-api-server/src/main/java/com/inzynierka2k24/apiserver/web/config/GlobalExceptionHandler.java
+++ apart-manager-api-server/src/main/java/com/inzynierka2k24/apiserver/web/config/GlobalExceptionHandler.java
@@ -5,47 +5,53 @@ import com.inzynierka2k24.apiserver.exception.reservation.ReservationNotFoundExc
 import com.inzynierka2k24.apiserver.exception.reservation.ReservationNotValidException;
 import com.inzynierka2k24.apiserver.exception.user.UserAlreadyExistsException;
 import com.inzynierka2k24.apiserver.exception.user.UserNotFoundException;
+import com.inzynierka2k24.apiserver.web.request.ApiErrorResponse;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.ErrorResponse;
-import org.springframework.web.ErrorResponseException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.server.ResponseStatusException;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler {
 
   @ExceptionHandler(UserAlreadyExistsException.class)
-  public ResponseEntity<ErrorResponse> handleUserAlreadyExistsException(
-      UserAlreadyExistsException e) {
-    return ResponseEntity.status(HttpStatus.BAD_REQUEST)
-        .body(new ErrorResponseException(HttpStatus.BAD_REQUEST, e));
+  public ResponseEntity<ApiErrorResponse> handleUserAlreadyExistsException(UserAlreadyExistsException e) {
+    return ResponseEntity.status(HttpStatus.CONFLICT)
+        .body(new ApiErrorResponse(HttpStatus.CONFLICT, e.getMessage()));
   }
 
   @ExceptionHandler(UserNotFoundException.class)
   public ResponseEntity<ErrorResponse> handleUserNotFoundException(UserNotFoundException e) {
     return ResponseEntity.status(HttpStatus.NOT_FOUND)
-        .body(new ErrorResponseException(HttpStatus.NOT_FOUND, e));
+        .body(new ResponseStatusException(HttpStatus.NOT_FOUND, e.getMessage()));
   }
 
   @ExceptionHandler(ApartmentNotFoundException.class)
   public ResponseEntity<ErrorResponse> handleApartmentNotFoundException(
       ApartmentNotFoundException e) {
     return ResponseEntity.status(HttpStatus.NOT_FOUND)
-        .body(new ErrorResponseException(HttpStatus.NOT_FOUND, e));
+        .body(new ResponseStatusException(HttpStatus.NOT_FOUND, e.getMessage()));
   }
 
   @ExceptionHandler(ReservationNotFoundException.class)
   public ResponseEntity<ErrorResponse> handleReservationNotFoundException(
       ReservationNotFoundException e) {
     return ResponseEntity.status(HttpStatus.NOT_FOUND)
-        .body(new ErrorResponseException(HttpStatus.NOT_FOUND, e));
+        .body(new ResponseStatusException(HttpStatus.CONFLICT, e.getMessage()));
   }
 
   @ExceptionHandler(ReservationNotValidException.class)
   public ResponseEntity<ErrorResponse> handleReservationNotValidException(
       ReservationNotValidException e) {
     return ResponseEntity.status(HttpStatus.BAD_REQUEST)
-        .body(new ErrorResponseException(HttpStatus.BAD_REQUEST, e));
+        .body(new ResponseStatusException(HttpStatus.BAD_REQUEST, e.getMessage()));
+  }
+
+  @ExceptionHandler(UserAlreadyExistsException.class)
+  public ResponseEntity<ApiErrorResponse> handleRuntimeException(RuntimeException e) {
+    return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+        .body(new ApiErrorResponse(HttpStatus.BAD_REQUEST, e.getMessage()));
   }
 }
