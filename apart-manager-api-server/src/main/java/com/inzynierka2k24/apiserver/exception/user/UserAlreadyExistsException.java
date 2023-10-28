7eea8e7b05caadd0fd61a62bcba14082350eec92
Krzysztof Szuba <krzysiu.sz120@gmail.com>
2023-10-28 16:04:55 +0200

    Change error msg

--- apart-manager-api-server/src/main/java/com/inzynierka2k24/apiserver/exception/user/UserAlreadyExistsException.java
+++ apart-manager-api-server/src/main/java/com/inzynierka2k24/apiserver/exception/user/UserAlreadyExistsException.java
@@ -3,9 +3,9 @@ package com.inzynierka2k24.apiserver.exception.user;
 public class UserAlreadyExistsException extends RuntimeException {
 
   public UserAlreadyExistsException() {
-    super("User with the same login already exists!");
+    super("User with the same login or email already exists!");
   }
   public UserAlreadyExistsException(Throwable e) {
-    super("User with the same login already exists!", e);
+    super("User with the same login or email already exists!", e);
   }
 }
