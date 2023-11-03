b6cb3ff0194bbd644d7f24c8cc42a4356c378420
SnicketPolska <snicketpolska@gmail.com>
2023-11-03 21:09:09 +0100

    Fix tests

--- apart-manager-api-web/src/generated/index.d.ts
+++ apart-manager-api-web/src/generated/index.d.ts
@@ -1,6 +1,6 @@
 /* tslint:disable */
 /* eslint-disable */
-// Generated using typescript-generator version 3.1.1185 on 2023-11-03 17:18:50.
+// Generated using typescript-generator version 3.1.1185 on 2023-11-03 21:08:03.
 
 export interface Apartment {
     id?: number;
