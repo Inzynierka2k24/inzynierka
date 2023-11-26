8ec331e3a3c68ce5f9b9ade578f98e333a98d706
Michał Kaźmierczak <mkazmierczak112@gmail.com>
2023-11-26 14:01:42 +0100

    Infrastructure Create deployable enviroment (#34)
    * Created basic Dockerfile
    
    * Adjusted Dockerfile
    
    * Added logging
    
    * Deploy without secrets
    
    * Updated token
    
    * Using secrets in Dockerfile
    
    * Created Dockerfile for local deployment
    
    * Updated dockerfile
    
    * Changed server port
    
    * Created action for Azure deployment
    
    * Building image from specific Dockerfile
    
    * Update deploy.yml
    
    * Update deploy.yml
    
    * Changed Azure region
    
    * Added exposed ports
    
    * Created job for deploying Messaging Service
    
    * Separate files for deployments
    
    * Changed location
    
    * Changed dns label
    
    * Added location to name
    
    * Changed dns
    
    * Passing secrets into envs
    
    * Test envs
    
    * Passing secrets to Docker build
    
    * Added context test
    
    * Added Dockerfile for ApartManager API Server
    
    * Passing secrets as secure envs
    
    * Removed unnecessary build commands
    
    * Added default values for envs
    
    * Removed default envs
    
    * Added quotas
    
    * Created pipeline for ApartManager API Server deployment
    
    * Feature/apart manager user edit (#35)
    
    * partial implementation
    
    * implementation of edit endpoint
    
    * update postman keycloak collection and Controller
    
    * fix logout behaviour
    
    * tests
    
    * Apartment list connected with backend
    
    * Frontend connected with backend
    
    * Adding missing files
    
    * Connecting with backend
    
    * Calendar works
    
    * Adjusting reservations
    
    * Adding db-schema
    
    * Adding reservations works
    
    * Edit finance has default values
    
    * ci: add Azure Static Web Apps workflow file
    on-behalf-of: @Azure opensource@microsoft.com
    
    * Updated app location
    
    * Updated app and output location
    
    * Installing modules
    
    * Added script
    
    * Added chart.js
    
    * Removed dependency
    
    * Pipeline for Auth Service
    
    * Changed command
    
    * Changed command
    
    * Moved command to Dockerfile entrypoint
    
    * Added chart.js
    
    * Removed chart.js from scripts
    
    * Changed budgets properties
    
    * Changed output location
    
    * Removed unnecessary code
    
    * Added output location
    
    * Changed output location
    
    * Changed output location
    
    * Changed output location
    
    * Changed output location
    
    * Changed output location
    
    * Changed output location
    
    * Added env to auth service deployment
    
    * Changed strict hostname to false for auth service deployment
    
    * Connecting web app with backend
    
    * Added secret
    
    * Added changeOrigin property
    
    * Added proxyConfig for prod config
    
    * Added cors config
    
    * Added env variable for api url
    
    * Using different cors config
    
    * Adding separate web config
    
    * Minor comment
    
    * Another cors setup
    
    * Minor cleanup
    
    ---------
    
    Co-authored-by: Michał Kaźmierczak <m.kazmierczak@sportradar.com>
    Co-authored-by: SnicketPolska <56560560+SnicketPolska@users.noreply.github.com>
    Co-authored-by: Krzysztof Maciejewski <94606867+kdmaciejewski@users.noreply.github.com>

--- apart-manager-api-web/src/app/apartment/services/apartment.service.ts
+++ apart-manager-api-web/src/app/apartment/services/apartment.service.ts
@@ -3,6 +3,7 @@ import {HttpClient, HttpEvent} from '@angular/common/http';
 import { Observable } from 'rxjs';
 import {Apartment} from "../../../generated";
 import {UserDTO} from "../../../generated";
+import { environment } from '../../../environments/environment';
 
 @Injectable({
   providedIn: 'root'
@@ -12,18 +13,18 @@ export class ApartmentService {
   constructor(private http: HttpClient) { }
 
   getApartments(user: UserDTO): Observable<Apartment[]> {
-    return this.http.get<Apartment[]>('/api/'+ user.id +'/apartment');
+    return this.http.get<Apartment[]>(`${environment.api_url}/`+ user.id +'/apartment');
   }
 
   addApartment(user: UserDTO, apartment: Apartment, options?: any): Observable<HttpEvent<string>> {
-    return this.http.post<string>('/api/'+ user.id +'/apartment', apartment, options);
+    return this.http.post<string>(`${environment.api_url}/`+ user.id +'/apartment', apartment, options);
   }
 
   updateApartment(user: UserDTO, apartment: Apartment): Observable<Apartment> {
-    return this.http.put<Apartment>('/api/'+ user.id +'/apartment/' + apartment.id, apartment);
+    return this.http.put<Apartment>(`${environment.api_url}/`+ user.id +'/apartment/' + apartment.id, apartment);
   }
 
   deleteApartment(user: UserDTO, apartmentId: number, options?: any): Observable<HttpEvent<void>> {
-    return this.http.delete<void>('/api/'+ user.id +'/apartment/' + apartmentId, options);
+    return this.http.delete<void>(`${environment.api_url}/`+ user.id +'/apartment/' + apartmentId, options);
   }
 }
