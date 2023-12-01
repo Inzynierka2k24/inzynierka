5f3b03c1a16d0076a29252c1bff366f1a4c9d85a
Michał Kaźmierczak <mkazmierczak112@gmail.com>
2023-12-01 21:43:44 +0100

    Fixing failing tests

--- apart-manager-api-server/src/test/java/com/inzynierka2k24/apiserver/service/IntegrationServiceTest.java
+++ apart-manager-api-server/src/test/java/com/inzynierka2k24/apiserver/service/IntegrationServiceTest.java
@@ -1,5 +1,6 @@
 package com.inzynierka2k24.apiserver.service;
 
+import static org.assertj.core.api.Assertions.*;
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
