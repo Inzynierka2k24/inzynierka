b21db89fb02d554024b276155981e9681cd41b18
Michał Kaźmierczak <mkazmierczak112@gmail.com>
2023-12-02 22:19:37 +0100

    Integrated getReservation and updateApartmentDetails endpoints

--- apart-manager-api-server/src/main/java/com/inzynierka2k24/apiserver/service/IntegrationService.java
+++ apart-manager-api-server/src/main/java/com/inzynierka2k24/apiserver/service/IntegrationService.java
@@ -1,13 +1,18 @@
 package com.inzynierka2k24.apiserver.service;
 
-import com.inzynierka2k24.Reservation;
 import com.inzynierka2k24.ServiceResponse;
 import com.inzynierka2k24.apiserver.exception.apartment.ApartmentNotFoundException;
 import com.inzynierka2k24.apiserver.exception.reservation.ReservationNotFoundException;
+import com.inzynierka2k24.apiserver.exception.reservation.ReservationNotValidException;
 import com.inzynierka2k24.apiserver.grpc.integration.ExternalIntegrationServiceClient;
+import com.inzynierka2k24.apiserver.model.EventType;
+import com.inzynierka2k24.apiserver.model.ExternalReservation;
+import com.inzynierka2k24.apiserver.model.Finance;
+import com.inzynierka2k24.apiserver.model.Source;
 import java.time.Instant;
 import java.util.List;
 import java.util.Map;
+import java.util.Optional;
 import java.util.stream.Collectors;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -21,6 +26,7 @@ public class IntegrationService {
   private final ExternalOfferService offerService;
   private final ReservationService reservationService;
   private final ApartmentService apartmentService;
+  private final FinanceService financeService;
 
   public Map<String, String> propagateReservation(long userId, long apartmentId, long reservationId)
       throws ReservationNotFoundException {
@@ -31,11 +37,35 @@ public class IntegrationService {
             offerService.getAll(apartmentId)));
   }
 
-  public List<Reservation> getReservations(
+  public List<ExternalReservation> getReservations(
       long userId, long apartmentId, Instant from, Instant to) {
-    client.getReservations(
-        from, to, accountService.getAll(userId), offerService.getAll(apartmentId));
-    return List.of();
+    var reservations =
+        client.getReservations(
+            from, to, accountService.getAll(userId), offerService.getAll(apartmentId), apartmentId);
+
+    reservations.forEach(
+        reservation -> {
+          try {
+            reservationService.add(apartmentId, reservation.reservation());
+          } catch (ReservationNotValidException e) {
+            throw new RuntimeException(e);
+          }
+          if (reservation.price().isPresent()) {
+            financeService.add(
+                new Finance(
+                    Optional.empty(),
+                    userId,
+                    apartmentId,
+                    0,
+                    EventType.RESERVATION,
+                    Source.BOOKING,
+                    reservation.price().get(),
+                    Instant.now(),
+                    "Reservation from external service"));
+          }
+        });
+
+    return reservations;
   }
 
   public Map<String, String> updateApartmentDetails(long userId, long apartmentId)
